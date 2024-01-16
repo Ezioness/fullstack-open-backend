72c6926456e066d1d53ff7af5803f502583ab0cd
Ezioness <romcas@gmx.fr>
2024-01-16 17:25:10 +0100

    uibuild

--- models/person.js
+++ models/person.js
@@ -4,8 +4,6 @@ mongoose.set('strictQuery', false)
 
 const url = process.env.MONGODB_URI
 
-console.log('Connecting to', url)
-
 mongoose.connect(url)
 	.then(result => console.log('Connected to MongoDB'))
 	.catch(error => console.log('Error connecting to MongoDB', error.message))
